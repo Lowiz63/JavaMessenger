c4c13b6b6aeea22a244fbe78a8b3e92f04c846bb
LudovicLAURICHESSE <33416114+LudovicLAURICHESSE@users.noreply.github.com>
2017-11-30 13:54:53 +0100

    Commit d'assemblage
    ListView
    BDD

--- Messenger/src/controller/MainWinController.java
+++ Messenger/src/controller/MainWinController.java
@@ -8,10 +8,15 @@ package controller;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Platform;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -19,11 +24,12 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuButton;
 import javafx.scene.image.Image;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
+import modèle.Utilisateur;
 
 /**
  *
@@ -32,19 +38,48 @@ import javafx.stage.Stage;
 public class MainWinController {
     @FXML    private Label lbPseudo;
     
-    @FXML    private ListView lvContact;
+    @FXML    private ListView<Utilisateur> lvContact= new ListView<>();
     
     @FXML    private MenuButton statut;
     
     @FXML    private Image imgetat;
     //pas effective
-    private ObservableList<String> contacts =FXCollections.observableArrayList ( "Ludo", "Thomas", "Loann");
+    ObservableList<Utilisateur> lcontacts= FXCollections.observableArrayList();
+    private ListProperty<Utilisateur> listContactProperty = new SimpleListProperty<>(lcontacts);
     
     @FXML
     public void initialize(URL url, ResourceBundle rb){
         lbPseudo.setText("Pseudo Contact"); 
-        //les contacts ne s'affiche pas
-        lvContact.setItems(contacts);
+        bindingLW();
+    }
+    
+    protected void bindingLW(){
+        lvContact.itemsProperty().bind(listContactProperty);        
+        
+        lvContact.setCellFactory(unused -> new ListCell<Utilisateur>(){
+            @Override
+            protected void updateItem(Utilisateur item, boolean empty) {
+                super.updateItem(item, empty);
+                if(!isEmpty()){
+                    textProperty().bind(item.pseudoProperty());
+                }else{
+                    textProperty().unbind();
+                    textProperty().setValue(null);
+                }
+            }
+        });
+        lvContact.getSelectionModel().selectedItemProperty()
+                .addListener(new ChangeListener<Utilisateur>() {
+                    public void changed(ObservableValue<? extends Utilisateur> observable,
+                                        Utilisateur oldValue, Utilisateur newValue) {
+                        if(newValue!=null)
+                            try {
+                                newChat();
+                        } catch (IOException ex) {
+                            Logger.getLogger(MainWinController.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                });
     }
     
     public void onExit(Event event) throws IOException{
@@ -99,7 +134,7 @@ public class MainWinController {
             stage.show();
     }
     
-    public void newChat(ActionEvent e) throws IOException{
+    public void newChat() throws IOException{
         Stage stage=new Stage();
         Parent root = FXMLLoader.load(getClass().getResource("/ihm/FenetreChat.fxml"));
         stage.setScene(new Scene(root));
